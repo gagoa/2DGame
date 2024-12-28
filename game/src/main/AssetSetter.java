a3a6f2190ffa21860a5176ecb2a2eb0d6ec8ae69
Armand Gago <gagoa@umich.edu>
2024-12-28 12:25:19 -0500

    Treasure hunting game

--- game/src/main/AssetSetter.java
+++ game/src/main/AssetSetter.java
@@ -15,35 +15,35 @@ public class AssetSetter {
 	public void setObject() {
 		
 		gp.obj[0] = new OBJ_Key(gp);
-		gp.obj[0].worldX = 10 * gp.tileSize;
-		gp.obj[0].worldY = 34 * gp.tileSize;
+		gp.obj[0].worldX = 23 * gp.tileSize;
+		gp.obj[0].worldY = 7 * gp.tileSize;
 		
 		gp.obj[1] = new OBJ_Key(gp);
-		gp.obj[1].worldX = 25 * gp.tileSize;
-		gp.obj[1].worldY = 34 * gp.tileSize;
+		gp.obj[1].worldX = 22 * gp.tileSize;
+		gp.obj[1].worldY = 41 * gp.tileSize;
 		
 		gp.obj[2] = new OBJ_Key(gp);
-		gp.obj[2].worldX = 35 * gp.tileSize;
-		gp.obj[2].worldY = 6 * gp.tileSize;
+		gp.obj[2].worldX = 38 * gp.tileSize;
+		gp.obj[2].worldY = 8 * gp.tileSize;
 		
 		gp.obj[3] = new OBJ_Door(gp);
-		gp.obj[3].worldX = 35 * gp.tileSize;
-		gp.obj[3].worldY = 8 * gp.tileSize;
+		gp.obj[3].worldX = 10 * gp.tileSize;
+		gp.obj[3].worldY = 12 * gp.tileSize;
 		
 		gp.obj[4] = new OBJ_Door(gp);
-		gp.obj[4].worldX = 35 * gp.tileSize;
-		gp.obj[4].worldY = 11 * gp.tileSize;
+		gp.obj[4].worldX = 12 * gp.tileSize;
+		gp.obj[4].worldY = 23 * gp.tileSize;
 		
 		gp.obj[5] = new OBJ_Door(gp);
-		gp.obj[5].worldX = 35 * gp.tileSize;
-		gp.obj[5].worldY = 13 * gp.tileSize;
+		gp.obj[5].worldX = 8 * gp.tileSize;
+		gp.obj[5].worldY = 28 * gp.tileSize;
 		
 		gp.obj[6] = new OBJ_Chest(gp);
-		gp.obj[6].worldX = 35 * gp.tileSize;
-		gp.obj[6].worldY = 16 * gp.tileSize;
+		gp.obj[6].worldX = 10 * gp.tileSize;
+		gp.obj[6].worldY = 9 * gp.tileSize;
 		
 		gp.obj[7] = new OBJ_Shoes(gp);
-		gp.obj[7].worldX = 12 * gp.tileSize;
-		gp.obj[7].worldY = 35 * gp.tileSize;
+		gp.obj[7].worldX = 35 * gp.tileSize;
+		gp.obj[7].worldY = 42 * gp.tileSize;
 	}
 }
